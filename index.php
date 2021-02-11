8f73634f7b731200b06496429c6d697ce484b3a5
GamuFP <victor.diez@campusfp.es>
2021-02-11 14:44:17 +0100

    contact hecho

--- index.php
+++ index.php
@@ -4,10 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="images\iconoInterciclicas.ico" /> 
+    <link rel="shortcut icon" href="images\iconoInterciclicas.ico" />
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Trocchi&display=swap" rel="stylesheet">
+
+
     <title>Web Interciclicas</title>
+    <script src="js/script.js"></script>
 </head>
 <body>
+    <?php
+    include_once("html/header.html");
+    ?>
+    <main>
+        <?php
+            include_once("html/section.html");
+        ?>
+    </main>
     
 </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+function element_pressed() {
+    for (let i = 0; i < arguments.length; i++) {
+        let element = document.getElementById(arguments[i]);
+        let state = element.className;
+        if (state == "unpressed") {
+            element.className = "pressed";
+        } else {
+            element.className = "unpressed";
+        }
+    }
+
+}
